02e48b06dfd8fc33718f2c88716b1a519042f482
Julio Barriga <mushachi@gmail>
2024-09-13 21:13:52 -0600

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CounterModule,
+    HeroesModule,
     AppRoutingModule
   ],
   providers: [],
